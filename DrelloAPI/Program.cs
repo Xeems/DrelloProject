a43b5b0065974cd83f1056c3382e2fd4401b95a0
roman krasilnikov <krasilnikovroman5@gmail.com>
2023-01-08 19:47:39 +0700

    bd initialize

--- DrelloAPI/Program.cs
+++ DrelloAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDBContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("SSQLConnection")));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLConnection")));
 
 var app = builder.Build();
 
