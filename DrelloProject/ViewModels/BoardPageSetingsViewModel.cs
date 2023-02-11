24dc8ac38f142f882bb26cc9dcff71886e5de8cd
roman krasilnikov <krasilnikovroman5@gmail.com>
2023-02-11 22:18:00 +0700

    board page settings

--- DrelloProject/ViewModels/BoardPageSetingsViewModel.cs
+++ DrelloProject/ViewModels/BoardPageSetingsViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DrelloProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DrelloProject.ViewModels
 {
-    public class BoardPageSetingsViewModel : ObservableObject
+    public partial class BoardPageSetingsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<BoardRole> roles = new ObservableCollection<BoardRole>();
+
+        [ObservableProperty]
+        private string boardName;
+
+        [ObservableProperty]
+        private string boardDescription;
+
+        [ObservableProperty]
+        private string roleName;
+
+        [ObservableProperty]
+        private BoardRole selectedRole;
+
+        [RelayCommand]
+        async void NewRoleBtn()
+        {
+            roles.Add(new BoardRole { Name = roleName });
+        }
+        
+        [RelayCommand]
+        async void DeleteRoleBtn()
+        {
+
+        }
     }
 }
