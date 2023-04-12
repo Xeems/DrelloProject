16b8add60ee549cf42dfafeb8408f2eddbf0bae3
Xeems <93206267+Xeems@users.noreply.github.com>
2023-04-12 10:33:22 +0700

    Delete DrelloApi directory

--- DrelloApi/Models/UserLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DrelloApi.Models
-{
-    public class UserLogin
-    {
-        public string? UserName { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
